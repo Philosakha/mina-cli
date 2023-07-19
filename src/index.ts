33bcda482cf031681014a95896979c7fd7b002a5
Saviour1001 <harshghodkar@gmail.com>
2023-07-20 02:15:07 +0530

    initiating the minacli

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+import chalk from "chalk";
+// @ts-ignore
+// const figlet = require('figlet');
+import figlet from "figlet";
+import createFrontend from "./creating-frontend";
+
+const availableOptions: string[] = [
+  "create-frontend",
+  "check-balance",
+  "check-transactions",
+];
+
+// second argument should be the selected option
+const option: string = process.argv[2];
+
+if (!availableOptions.includes(option)) {
+  console.log(
+    chalk.red(
+      `Invalid operation. Available operations are: ${availableOptions}`
+    )
+  );
+  process.exit(-1);
+}
+
+console.log(
+  chalk.magentaBright(figlet.textSync(`Mina`, { horizontalLayout: "full" }))
+);
+
+switch (option) {
+  case "create-frontend":
+    console.log(chalk.green("Creating frontend..."));
+    createFrontend();
+    break;
+  case "check-balance":
+    console.log(chalk.green("Checking balance..."));
+    break;
+  case "check-transactions":
+    console.log(chalk.green("Checking transactions..."));
+    break;
+}
