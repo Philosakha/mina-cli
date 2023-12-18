04803eb040e26b464fa041f954947b22f0ca9a16
Saviour1001 <harshghodkar@gmail.com>
2023-12-18 23:58:47 +0530

    testing

--- src/check-balance.ts
+++ src/check-balance.ts
@@ -52,6 +52,8 @@ export default async function checkBalance() {
 
   let data = await response.text();
 
+  console.log(data);
+
   load.stop();
 
   let dataJSON = JSON.parse(data) as minaAccountsData;
